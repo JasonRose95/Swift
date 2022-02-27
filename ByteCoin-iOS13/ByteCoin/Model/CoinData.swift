70e689ae99b824bb3434e59dcc7e1c64f2074f20
Jason Rosenthal <jasonrosenthalr@gmail.com>
2022-02-26 19:18:29 -0800

    ByteCoin

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Jason Rosenthal on 2/26/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
